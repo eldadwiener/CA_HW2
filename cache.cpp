8e403a6c23dc1bf45335c3d3b07b96135a4c810b
Eldad Wiener <EldadWiener@gmail.com>
2019-04-26 13:51:27 +0300

    add logic for victim

--- cache.cpp
+++ cache.cpp
@@ -25,10 +25,8 @@ void cacheSet::write(uint32_t tag, uint32_t offset) {
 
 // throws evicted block if any
 void cacheSet::insert(uint32_t tag) {
-
 	if (_ways.size() < _waySize) {
 		_ways.push_back(cacheBlock(tag));
-		_waySize++;
 		return;
 	}
 	cacheBlock temp = _ways.front();
@@ -48,5 +46,32 @@ list<cacheBlock>::iterator cacheSet::find(uint32_t tag) {
 	}
 	return itr;
 }
+
+//victim c'tor
+victim::victim(uint32_t blockSize) : _blockSize(blockSize), _blocks() {}
+// throws block or NOTFOUND exception
+void victim::get(uint32_t tag) {
+	list<cacheBlock>::iterator itr = _blocks.begin();
+	while (itr != _blocks.end())
+	{
+		if (itr->tag == tag) {
+			throw itr;
+		}
+		itr++;
+	}
+	throw notfound;
+}
+
+// throws evicted block if any TODO: is this needed?
+void victim::insert(uint32_t tag){
+	if (_blocks.size() < VICTIMSIZE) {
+		_blocks.push_back(cacheBlock(tag));
+		return;
+	}
+	_blocks.pop_front;
+	_blocks.push_back(cacheBlock(tag));
+	//TODO maybe need to update somthing
+}
+
 
 
